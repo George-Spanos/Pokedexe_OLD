576a43e2b8f0445f266da395c97aea7a7bae476c
George Spanos <gespa11019@gmail.com>
2021-10-28 03:38:40 +0300

    Stylings and icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   purge: {
     enabled: true,
@@ -7,9 +8,33 @@ module.exports = {
     ],
   },
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
+    theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        colors: {
+            gray: colors.coolGray,
+            blue: colors.lightBlue,
+            red: colors.rose,
+            pink: colors.fuchsia,
+        },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {
+            spacing: {
+                '128': '32rem',
+                '144': '36rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            }
+        }
+    },
   variants: {
     extend: {},
   },
