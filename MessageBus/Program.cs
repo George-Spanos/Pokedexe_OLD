c6f8c42929063ef96af07b697dbdde4496896d2c
George Spanos <gespa11019@gmail.com>
2021-10-31 12:14:21 +0200

    Test commit for deploy

--- MessageBus/Program.cs
+++ MessageBus/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-
 namespace MessageBus {
     public class Program {
         public static void Main(string[] args)
