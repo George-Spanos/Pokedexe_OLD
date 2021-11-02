1a950559a765fdc805b802cdd2fdaa5335679e94
gspanos <gspanos@inttrust.gr>
2021-11-02 17:25:04 +0200

    Github workflows

--- Web/Features/Chat/MessageList.razor.cs
+++ Web/Features/Chat/MessageList.razor.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.JSInterop;
+using Model.Extensions;
 using PokedexChat.Data;
 using Proto;
 namespace PokedexChat.Features.Chat {
@@ -17,11 +20,14 @@ namespace PokedexChat.Features.Chat {
         [Inject]
         private IDataService DataService { get; set; }
 
-        protected IEnumerable<Message> Messages { get; private set; }
+        protected ImmutableList<ImmutableList<Message>> Messages { get; private set; }
 
         protected override void OnInitialized()
         {
-            Messages = DataService.Messages;
+            Messages = DataService.Messages
+                .GroupWhile((next, previous) => next.UserEmail == previous.UserEmail)
+                .Select(m => m.ToImmutableList())
+                .ToImmutableList();
         }
         protected override async void OnAfterRender(bool firstRender)
         {
