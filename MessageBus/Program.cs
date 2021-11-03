7bee36d3326aa952b936ee0c7caf68858a06b2e7
gspanos <gspanos@inttrust.gr>
2021-11-03 13:46:35 +0200

    Message Bus Deploy Test

--- MessageBus/Program.cs
+++ MessageBus/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 namespace MessageBus {
     public class Program {
@@ -12,6 +13,7 @@ namespace MessageBus {
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.ConfigureKestrel(options => options.ListenAnyIP(443, o => o.Protocols = HttpProtocols.Http2));
                     webBuilder.UseStartup<Startup>();
                 });
     }
