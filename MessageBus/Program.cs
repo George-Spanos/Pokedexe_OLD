4b8bbc83c01b0f02bc5c269719ac6f4878614a5a
George Spanos <gespa11019@gmail.com>
2021-11-04 02:43:08 +0200

    Serilog fixes message bus

--- MessageBus/Program.cs
+++ MessageBus/Program.cs
@@ -2,13 +2,16 @@ using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
 namespace MessageBus {
     public class Program {
         public static int Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
-                .ReadFrom.AppSettings()
-                .CreateLogger();
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
             try{
                 Log.Information("Starting web host");
                 CreateHostBuilder(args).Build().Run();
@@ -27,7 +30,9 @@ namespace MessageBus {
         // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog()
+                .UseSerilog((context, _, configuration) =>
+                    configuration
+                        .ReadFrom.Configuration(context.Configuration))
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
                 });
